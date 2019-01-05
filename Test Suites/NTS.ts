20e6ea15150cd2129a7d73557100fb4428822f22
Coner <Coner@DESKTOP-3SBFI01>
2019-01-05 15:55:43 +0200

    New player in town

---
+++ Test Suites/NTS.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5e1708dc-2029-4b71-a716-32a4716182e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb9fa920-6217-42be-8a34-09a54ab3c704</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reborn</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
